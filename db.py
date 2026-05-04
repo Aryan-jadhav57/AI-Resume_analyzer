e141a16d998ec998aef513c7475a9d869ef34e73
Aryan Jadhav <aryan.p.jadhavv@gmail.com>
2026-05-04 09:53:37 +0530

    Modify database connection string for security
    Updated DATABASE_URL to hide the password.

--- db.py
+++ db.py
@@ -3,7 +3,7 @@ from sqlalchemy import create_engine
 # sqlalchemy.orm is used to raw code is used for database and declarative base is used as base classes for tables and session maker is used for session,read
 from sqlalchemy.orm import declarative_base, sessionmaker  
 
-DATABASE_URL = "mysql+pymysql://2SkfjRTzvkJdFfE.root:M9vDcq7xECfxAm8T@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test"
+DATABASE_URL = "mysql+pymysql://2SkfjRTzvkJdFfE.root:<password>@gateway01.us-east-1.prod.aws.tidbcloud.com:4000/test"
 
 engine = create_engine(    # create databaase connection
   DATABASE_URL,            #use the above link
